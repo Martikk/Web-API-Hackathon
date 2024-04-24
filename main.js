991a2bdefbe9aaf4c1a609ce2e793eec57de256e
your_name <alex.martishyn@gmail.com>
2024-04-24 14:04:08 -0700

    done_style

--- main.js
+++ main.js
@@ -1,44 +1,12 @@
 import DadJokeApi from "./scripts/dad-joke-api.js";
 
-
-// let test = new DadJokeApi();
-
-// const dadJokeContainerEl = document.querySelector(".dad-joke__container");
-
-// const dadJokeEl = document.createElement("h1");
-
-// async function createJoke() {
-//     const joke = await test.getDadJoke();
-//     dadJokeEl.innerText = joke;
-//     dadJokeContainerEl.append(dadJokeEl);
-// }
-
-
-// createJoke();
-
-
-    const dadJokeContainerEl = document.querySelector(".dad-joke__container");
-
-    const dadJokeEl = document.createElement("p");
-    dadJokeEl.classList.add("dad-joke__text");
+async function refreshDadJoke() {     
+    const dadJokeEl = document.querySelector(".dad-joke__text");
 
     dadJokeEl.innerText = await new DadJokeApi().getDadJoke();
-
-    dadJokeContainerEl.append(dadJokeEl);
-
-
-async function refreshDadJoke(e) {
-    e.preventDefault();
-
-    const dadJokeContainerEl = document.querySelector(".dad-joke__container");
-     
-    const dadJokeEl = document.querySelector("dad-joke__text");
-
-    dadJokeEl.innerText = await new DadJokeApi().getDadJoke();
-
-    dadJokeContainerEl.append(dadJokeEl);
 }
-
 
 const buttonEl = document.querySelector(".btn--new-joke");
 buttonEl.addEventListener("click", refreshDadJoke);
+
+refreshDadJoke();
--- scripts/weatherScript.js
+++ scripts/weatherScript.js
@@ -1,2 +1,39 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const apiUrl = 'http://www.7timer.info/bin/astro.php?lon=113.17&lat=23.09&ac=0&lang=en&unit=metric&output=json&tzshift=0';
+const settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "http://dataservice.accuweather.com/currentconditions/v1/53286?details=true&apikey=LBpc8o23TjwLcElAgBf7GWzwCGMzZPVG",
+    "method": "GET",
+    "headers": {
+      "Accept": "*/*",
+      "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+      "Authorization": "Basic c3RlZmFucmVlOndlYXRoZXI=LBpc8o23TjwLcElAgBf7GWzwCGMzZPVG"
+    }
+  };
+  
+    console.log(settings);
+
+    
+const url = "http://dataservice.accuweather.com/currentconditions/v1/";
+const apiKey = "LBpc8o23TjwLcElAgBf7GWzwCGMzZPVG";
+
+const fetchSettings = {
+    method: "GET",
+    headers: {
+        "Accept": "application/json",
+    }
+};
+function getWeather(locationKey) {
+    const fullUrl =`${url}${locationKey}?apikey=${apikey}`;
+
+    fetch (fullUrl, fetchSettings)
+    .then (Response => {
+        if (!Response.ok) {
+            throw new Error ('Today weather for you awful;'+ Response.statusText);
+        }
+        return response.json();
+    })
+    .catch (error => {
+        console.error ('Fetch error:', error);
+    });
+}
+getWeather ('New')
