03b3dec2e32ee75c14f54ec7fd5343144e4bc0e7
your_name <alex.martishyn@gmail.com>
2024-04-24 13:28:59 -0700

    add_readme_file

--- main.js
+++ main.js
@@ -1,15 +1,44 @@
 import DadJokeApi from "./scripts/dad-joke-api.js";
 
-let test = new DadJokeApi();
 
-const dadJokeContainerEl = document.querySelector(".dad-joke__container");
+// let test = new DadJokeApi();
 
-const dadJokeEl = document.createElement("h1");
+// const dadJokeContainerEl = document.querySelector(".dad-joke__container");
 
-async function createJoke() {
-    const joke = await test.getDadJoke();
-    dadJokeEl.innerText = joke;
+// const dadJokeEl = document.createElement("h1");
+
+// async function createJoke() {
+//     const joke = await test.getDadJoke();
+//     dadJokeEl.innerText = joke;
+//     dadJokeContainerEl.append(dadJokeEl);
+// }
+
+
+// createJoke();
+
+
+    const dadJokeContainerEl = document.querySelector(".dad-joke__container");
+
+    const dadJokeEl = document.createElement("p");
+    dadJokeEl.classList.add("dad-joke__text");
+
+    dadJokeEl.innerText = await new DadJokeApi().getDadJoke();
+
+    dadJokeContainerEl.append(dadJokeEl);
+
+
+async function refreshDadJoke(e) {
+    e.preventDefault();
+
+    const dadJokeContainerEl = document.querySelector(".dad-joke__container");
+     
+    const dadJokeEl = document.querySelector("dad-joke__text");
+
+    dadJokeEl.innerText = await new DadJokeApi().getDadJoke();
+
     dadJokeContainerEl.append(dadJokeEl);
 }
 
-createJoke();
+
+const buttonEl = document.querySelector(".btn--new-joke");
+buttonEl.addEventListener("click", refreshDadJoke);
---
+++ scripts/weatherScript.js
@@ -0,0 +1,2 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const apiUrl = 'http://www.7timer.info/bin/astro.php?lon=113.17&lat=23.09&ac=0&lang=en&unit=metric&output=json&tzshift=0';
